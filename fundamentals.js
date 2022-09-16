e7a2b51260fc6b666a538424ae8a6e93419f9daa
Andrew Baxter <andrewbaxter92@gmail.com>
2022-09-16 14:55:53 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -15,6 +15,21 @@ const aCar = {
       number : 1058
     }
 
+
     
   }
+
+  aCar.mileage = 10000;
+  aCar.colour = {
+    extColour : "red",
+    interior : {
+      texture : 'leather',
+      shade : 'cream'
+    } 
+
+
+  }
   console.log('Reg. = ' + aCar.registration.year + '-' + aCar.registration.countyCode + '-' + aCar.registration.number)
+  console.log('It is a ' + aCar.colour.extColour + ' car, ' + aCar.mileage + ' mileage, with ' + aCar.colour.interior.texture + " interior")
+
+
